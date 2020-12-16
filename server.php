77bb07aaccf9a544f42abb331e3a9ec794e79c14
Salamandra19977 <60927007+Salamandra19977@users.noreply.github.com>
2020-12-16 16:18:45 +0200

    2.0

--- server.php
+++ server.php
@@ -8,9 +8,25 @@
 
 		public function getFileFromName($file) 
 		{
-			if($file == "employees.xml") {
+			$employees = new SimpleXMLElement('employees.xml',  null, true);
+			$employees->employee[0]->firstname;
 
-				return file_get_contents('employees.xml');
+			if($file['name'] == "employees.xml") {
+				$output = null;
+				
+				for($i = 0; $i < $file['count']; $i++) {
+					$output .= "*************************"."\r\n";
+					$output .= "Працівник id".$i."\r\n";
+					if($file['firstname']) $output .= "Ім'я: ".$employees->employee[$i]->firstname."\r\n";
+					if($file['lastname']) $output .= "Прізвище: ".$employees->employee[$i]->lastname."\r\n";
+					if($file['title']) $output .= "Підрозділ: ".$employees->employee[$i]->title."\r\n";
+					if($file['division']) $output .= "Посада: ".$employees->employee[$i]->division."\r\n";
+					if($file['building']) $output .= "Корпус: ".$employees->employee[$i]->building."\r\n";
+					if($file['room']) $output .= "Номер робочої кімнати: ".$employees->employee[$i]->room."\r\n";
+					$output .= "*************************"."\r\n";
+				}
+
+				return $output;
 			}
 			else {
 
@@ -18,7 +34,6 @@
 			}
 		}
 	}
-
 	$params = array('uri' => 'rksm4.loc/server.php');
 	$server = new SoapServer(NULL,$params);
 	$server->setClass('Server');
--- service.php
+++ service.php
@@ -1,6 +1,26 @@
 <?php 
 	include 'client.php';
 	header('Content-type: text/plain');
-	$file = array('name' => $_GET['name_file']);;
+	$name = $_GET['name_file'];
+	$count = $_GET['count'];
+	$firstname = $_GET['option1'];
+	$lastname = $_GET['option2'];
+    $title = $_GET['option3'];
+    $division = $_GET['option4'];
+    $building = $_GET['option5'];
+    $room = $_GET['option6'];
+
+	$file = array(
+		'file' => array(		
+			'name' => $name,
+			'count' => $count,
+			'firstname' => $firstname,
+			'lastname' => $lastname,
+			'title' => $title,
+			'division' => $division,
+			'building' => $building,
+			'room' => $room
+		)
+	);
 	echo $client->getFile($file);
 ?>
